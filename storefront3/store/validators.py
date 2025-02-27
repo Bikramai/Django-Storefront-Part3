8976c05cd83214c65f493d553a863137d74c34c7
bikramia <bikram.rhce@gmail.com>
2025-02-27 15:02:13 -0500

    Uploading  Files -Enabling CORS

--- storefront3/store/validators.py
+++ storefront3/store/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_file_size(value):
-    max_size_kb = 50
+    max_size_kb = 1024
 
     if value.size > max_size_kb * 1024:
         raise ValidationError(f'Files cannot be larger than {max_size_kb} KB!')
